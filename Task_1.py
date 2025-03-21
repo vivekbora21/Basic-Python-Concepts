ee09d655e9b973a616d96d8cbc9cc4c58ce74471
vivekbora21 <108716812+vivekbora21@users.noreply.github.com>
2025-03-21 11:45:51 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the second number :"))
+
+print("Addition:",(num1+num2))
+print("Subtraction:",(num1-num2))
+print("Multipication:",(num1*num2))
+print("Division:",(num1/num2))
---
+++ Task_2.py
@@ -0,0 +1,4 @@
+name = input("Enter your first name:")
+lname = input("Enter your lastt name:")
+
+print(f"Hello {name} {lname}! Welcome to the Python program.")
